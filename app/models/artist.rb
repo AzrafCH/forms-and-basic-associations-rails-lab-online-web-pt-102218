5e772ee61f8cf8f84c0ba064f83c4f40fa526743
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-23 07:09:39 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,13 @@ class Artist < ActiveRecord::Base
   # add associations here
   has_many :songs
   has_many :genres, through: :songs
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end 
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end 
+    
 end
