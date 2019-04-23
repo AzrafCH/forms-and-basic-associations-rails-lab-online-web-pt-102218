3160529d94708968406dd3dc696ad6f14ac34986
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-23 07:18:14 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,9 +2,9 @@ class Note < ActiveRecord::Base
   # add associations here
   belongs_to :songs
 
-  def note_contents=(notes)
-    notes.each do |note|
-      notes = Song.find(note)
+  def note_contents=(ids)
+    ids.each do |id|
+      notes = Song.find(id)
       self.songs << notes
     end
   end
